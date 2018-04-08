ae63c63520f10a9c508538aa90eee1dc8d289433
CoderDennis <dodava@gmail.com>
2018-04-08 11:05:47 -0500

    Empty square shows a number; showGrid is working

--- tic-tac-toe.hs
+++ tic-tac-toe.hs
@@ -1,12 +1,12 @@
 import Data.List
 import Data.List.Split
 
-data Square = X | O | Empty
+data Square = X | O | Empty Int
 
 showSquare :: Square -> String
 showSquare X = "X"
 showSquare O = "O"
-showSquare Empty = " "
+showSquare (Empty i) = show i
 
 data Grid = List Square
 
@@ -14,12 +14,11 @@ showGrid :: [Square] -> [String]
 showGrid grid = 
   let lines = chunksOf 3 grid
   in 
-    map showGridLine lines
+    intersperse "---------" (map showLine lines)
 
-showGridLine = intercalate "|" . map showSquare
+showLine :: [Square] -> String
+showLine = intercalate " | " . map showSquare
 
 tic_tac_toe = do
-  let grid = [Empty, Empty, Empty,
-          Empty, Empty, Empty,
-          X, O, X]
-  show (showGrid grid)
+  let grid = [Empty i | i <- [0..8]]
+  mapM_ putStrLn (showGrid grid)
