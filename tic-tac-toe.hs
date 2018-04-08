11f6cde9323132a8d47d87402b841e3326cffd00
CoderDennis <dodava@gmail.com>
2018-04-08 13:14:14 -0500

    removes need for external package

--- tic-tac-toe.hs
+++ tic-tac-toe.hs
@@ -1,5 +1,4 @@
 import Data.List
-import Data.List.Split
 
 data Square = X | O | Empty Char deriving (Eq)
 
@@ -11,6 +10,12 @@ showSquare (Empty i) = [i]
 showGrid :: [Square] -> IO ()
 showGrid grid =
   mapM_ putStrLn (showGridLines grid)
+
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf _ [] = []
+chunksOf n l
+  | n > 0 = (take n l) : (chunksOf n (drop n l))
+  | otherwise = error "n must be greater than zero"
 
 showGridLines :: [Square] -> [String]
 showGridLines grid = 
@@ -41,7 +46,7 @@ turn grid player =
 
 setSquare :: [Square] -> Square -> Char -> [Square]
 setSquare grid player squareChar =
-  let [x,ys] = splitWhen (\sq -> matchSquare sq squareChar) grid
+  let (x,_:ys) = break (\sq -> matchSquare sq squareChar) grid
    in x ++ player : ys
       
 matchSquare :: Square -> Char -> Bool
