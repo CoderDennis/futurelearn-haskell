9ae4b107017822af82a861c0665449b6a30b5651
CoderDennis <dodava@gmail.com>
2018-04-14 17:48:54 -0500

    adds type definitions

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,14 +1,19 @@
 import Data.List
 
+fizzbuzz :: IO ()
 fizzbuzz =
   mapM_ putStrLn $ take 100 infinitefizzbuzz
 
+showLine :: (Int, String, String) -> String
 showLine (x, "", "") = show x
 showLine (_, f, b) = f ++ b
 
+infinitefizzbuzz :: [String]
 infinitefizzbuzz = 
   map showLine $ zip3 [1,2..] fizz buzz
 
+fizz :: [String]
 fizz = cycle(["","","fizz"])
 
+buzz :: [String]
 buzz = cycle(["","","","","buzz"])
