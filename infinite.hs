28ef6e37ca3e6a95a9dbe5cccf5e03db7f4abfab
CoderDennis <dodava@gmail.com>
2018-04-24 11:31:54 -0500

    further optimizations

--- infinite.hs
+++ infinite.hs
@@ -2,13 +2,13 @@ import Data.List
 
 -- optimized versions of functions to produce infinite list of prime numbers
 
-properfactors' :: Int -> [Int]
-properfactors' x = filter (\y -> (x `mod` y == 0)) [2..(x `div` 2)] -- only need to check up to half of x
+hasproperfactor :: Int -> Bool -- returns True as soon as it finds a factor
+hasproperfactor x = any (\y -> (x `mod` y == 0)) [3..(x `div` 2)] -- only need to check up to half of x
 
 primes' :: [Int]
 primes' = 2 : -- start with 2, which is the only even prime number
   (filter 
-    (\x -> null (properfactors' x)) -- using null instead of length prevents traversing the list
+    (\x -> not (hasproperfactor x))
     [3,5..] -- only check odd numbers starting with 3
   )
 
